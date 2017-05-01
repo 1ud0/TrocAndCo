0c42569278dab370c3384c0a73224c856e70f5cd
Cyril <Cyril@DESKTOP-CDTELH6.form-domain.lan>
2017-05-01 18:30:19 +0200

    Creation Dao et IDao favori, manque la recuperation de la liste des favoris d'un membre (il faut des données dans la base pour tester)

---
+++ TAC_POM/TAC_Data_API/src/main/java/com/tac/data/api/IDaoFavori.java
@@ -0,0 +1,14 @@
+package com.tac.data.api;
+
+import java.util.List;
+
+import com.tac.entity.Membre;
+import com.tac.entity.Proposition;
+
+public interface IDaoFavori {
+
+	Proposition addFavori(Proposition proposition, Membre membre);
+	void deleteFavori(Proposition proposition, Membre membre);
+	void deleteAllFavori(Proposition proposition);
+	List<Proposition> getFavorisByMembre(Membre membre);
+}
---
+++ TAC_POM/TAC_Data_EJB/src/main/java/com/tac/data/impl/DaoFavori.java
@@ -0,0 +1,56 @@
+package com.tac.data.impl;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.tac.data.api.IDaoFavori;
+import com.tac.entity.Membre;
+import com.tac.entity.Proposition;
+
+@Remote(IDaoFavori.class)
+@Stateless
+public class DaoFavori implements IDaoFavori {
+
+	@PersistenceContext(unitName = "TAC_Data_EJB")
+	EntityManager em;
+
+	@Override
+	public Proposition addFavori(Proposition proposition, Membre membre) {
+		String req = "INSERT INTO Favori (Membre_idMembre, Proposition_idProposition) values (:pidMembre, :pidProposition)";
+		Query query = em.createQuery(req);
+		query.setParameter("pidMembre", membre.getIdMembre()).setParameter("pidProposition",
+				proposition.getIdProposition());
+		return proposition;
+	}
+
+	@Override
+	public void deleteFavori(Proposition proposition, Membre membre) {
+		String req = "DELETE FROM Favori  WHERE Membre_idMembre = :pidMembre "
+				+ "AND Proposition_idProposition = :pidProposition";
+		Query query = em.createQuery(req);
+		query.setParameter("pidMembre", membre.getIdMembre()).setParameter("pidProposition",
+				proposition.getIdProposition());
+
+	}
+
+	@Override
+	public void deleteAllFavori(Proposition proposition) {
+		String req = "DELETE FROM Favori  WHERE Proposition_idProposition = :pidProposition ";
+		Query query = em.createQuery(req);
+		query.setParameter("pidProposition", proposition.getIdProposition());
+	}
+
+	
+	@Override
+	public List<Proposition> getFavorisByMembre(Membre membre) {
+		
+		return membre.getFavoris();
+		
+	}
+
+}
