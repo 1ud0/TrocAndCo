1ffd76a5375f495de3bcf91c3a2d8c974f737bc4
Cyril <Cyril@DESKTOP-CDTELH6.form-domain.lan>
2017-05-02 17:10:32 +0200

    Test et correction des query de DaoFavori

--- TAC_POM/TAC_Data_EJB/src/main/java/com/tac/data/impl/DaoFavori.java
+++ TAC_POM/TAC_Data_EJB/src/main/java/com/tac/data/impl/DaoFavori.java
@@ -21,8 +21,8 @@ public class DaoFavori implements IDaoFavori {
 
 	@Override
 	public Proposition addFavori(Proposition proposition, Membre membre) {
-		String req = "INSERT INTO Favori (Membre_idMembre, Proposition_idProposition) values (:pidMembre, :pidProposition)";
-		Query query = em.createQuery(req);
+		String req = "INSERT INTO Favori (membre_id, proposition_id) values (:pidMembre, :pidProposition)";
+		Query query = em.createNativeQuery(req);
 		query.setParameter("pidMembre", membre.getIdMembre()).setParameter("pidProposition",
 				proposition.getIdProposition());
 		query.executeUpdate();
@@ -31,9 +31,8 @@ public class DaoFavori implements IDaoFavori {
 
 	@Override
 	public void deleteFavori(Proposition proposition, Membre membre) {
-		String req = "DELETE FROM Favori  WHERE Membre_idMembre = :pidMembre "
-				+ "AND Proposition_idProposition = :pidProposition";
-		Query query = em.createQuery(req);
+		String req = "DELETE FROM Favori  WHERE Membre_id = :pidMembre " + "AND Proposition_id = :pidProposition";
+		Query query = em.createNativeQuery(req);
 		query.setParameter("pidMembre", membre.getIdMembre()).setParameter("pidProposition",
 				proposition.getIdProposition());
 		query.executeUpdate();
@@ -42,19 +41,18 @@ public class DaoFavori implements IDaoFavori {
 
 	@Override
 	public void deleteAllFavori(Proposition proposition) {
-		String req = "DELETE FROM Favori  WHERE Proposition_idProposition = :pidProposition ";
-		Query query = em.createQuery(req);
+		String req = "DELETE FROM Favori  WHERE Proposition_id = :pidProposition ";
+		Query query = em.createNativeQuery(req);
 		query.setParameter("pidProposition", proposition.getIdProposition());
 		query.executeUpdate();
 
 	}
 
-	
 	@Override
 	public List<Proposition> getFavorisByMembre(Membre membre) {
-		
-		return membre.getFavoris();
-		
+
+		return em.merge(membre).getFavoris();
+
 	}
 
 }
