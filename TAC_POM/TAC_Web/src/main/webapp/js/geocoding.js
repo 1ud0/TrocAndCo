f96cf6db73413039f82fba94d880b69e1d85913f
1ud0 <ludoviccadet74@gmail.com>
2017-05-04 13:47:41 +0200

    nettoyage geocoding js

--- TAC_POM/TAC_Web/src/main/webapp/js/geocoding.js
+++ TAC_POM/TAC_Web/src/main/webapp/js/geocoding.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-	var form = document.getElementById("formInscription");
 	var btnVisible = document.getElementById("btnVisible");
 	var btnCache = document.getElementById("formInscription:btn");
 	btnVisible.onclick = function() {
-		console.log("coucou");
 		var geocoder = new google.maps.Geocoder();
 		var chpLatitude = document.getElementById("formInscription:latitude");
 		var chpLongitude = document.getElementById("formInscription:longitude");
@@ -12,13 +10,10 @@ window.onload = function() {
 		var chpVille = document.getElementById("formInscription:ville");
 		//recupération de l'adresse depuis les 3 imput text
 	    var address = chpAdresse.value + ", " + chpCodePostal.value + ", " + chpVille.value;
-	    console.log(address);
 	    //geocoding
 	    geocoder.geocode({'address': address}, function(results, status) {
 	        if (status === google.maps.GeocoderStatus.OK) {
 	            var maLocalisation = results[0].geometry.location;
-	            console.log(results[0].geometry.location.lat());
-	            console.log(results[0].geometry.location.lng());
 	            chpLatitude.value = results[0].geometry.location.lat();
 	            chpLongitude.value = results[0].geometry.location.lng();
 	            btnCache.click();
