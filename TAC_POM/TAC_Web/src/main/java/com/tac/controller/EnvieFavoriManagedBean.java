98852ca0fa4559fe19831c4b72b070065951923f
Cyril <Cyril@DESKTOP-CDTELH6.form-domain.lan>
2017-05-06 12:49:44 +0200

    modif scoped

--- TAC_POM/TAC_Web/src/main/java/com/tac/controller/EnvieFavoriManagedBean.java
+++ TAC_POM/TAC_Web/src/main/java/com/tac/controller/EnvieFavoriManagedBean.java
@@ -6,8 +6,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
-
+import javax.faces.bean.SessionScoped;
 import com.tac.business.api.IServiceEnvie;
 import com.tac.business.api.IServiceFavori;
 import com.tac.business.api.IServiceListe;
@@ -19,7 +18,7 @@ import com.tac.entity.Membre;
 import com.tac.entity.Proposition;
 
 @ManagedBean(name = "mbEnvieFavori")
-@ViewScoped
+@SessionScoped
 public class EnvieFavoriManagedBean {
 
 	@EJB
